975bc06bf404f37b12b1e3b772f7acf3a6bcd0b4
unknown <generationsamosa@gmail.com>
2022-02-19 14:55:27 -0500

    Added Text Recognition

--- app/src/main/java/com/example/apppill/HomePage.java
+++ app/src/main/java/com/example/apppill/HomePage.java
@@ -8,15 +8,18 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -27,18 +30,30 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
+
 public class HomePage extends AppCompatActivity implements View.OnClickListener{
 
     private Button dictate;
     private TextView banner, description;
+    private String resultText;
     private ImageView display;
     private ActivityResultLauncher launcher;
-
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,10 +70,23 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener{
         launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             @Override
             public void onActivityResult(ActivityResult result) {
+                Toast.makeText(HomePage.this, "Hihi", Toast.LENGTH_SHORT).show();
                 if (result.getResultCode() == RESULT_OK && result.getData() != null){
                     Bundle bundle = result.getData().getExtras();
                     Bitmap bitmap = (Bitmap) bundle.get("data");
                     bitmap = rotateBitmap(bitmap, 90);
+                    TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+                    InputImage image = InputImage.fromBitmap(bitmap, 0);
+                    Task<Text> processedImage =
+                            recognizer.process(image)
+                                    .addOnSuccessListener(new OnSuccessListener<Text>() {
+                                        @Override
+                                        public void onSuccess(Text text) {
+                                            resultText = text.getText();
+                                            Toast.makeText(HomePage.this, "Processed Image :D", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                    Toast.makeText(HomePage.this, "So it processed the image", Toast.LENGTH_SHORT).show();
                     display.setImageBitmap(bitmap);
                 }
             }
@@ -70,6 +98,18 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener{
         switch (view.getId()){
             case R.id.homePageImage:
                 takePictureAndDisplay();
+                break;
+            case R.id.homePageDictate:
+                if (resultText != null){
+                    description.setText(resultText.toString());
+                }
+                else{
+                    Toast.makeText(HomePage.this, "Please take an image", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.homePageLogo:
+                startActivity(new Intent(HomePage.this, MainActivity.class));
+                break;
         }
     }
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -78,9 +118,12 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener{
         if (ContextCompat.checkSelfPermission(HomePage.this,
                 Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[]{
-                    Manifest.permission.CAMERA}, 225);
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE}, 225);
         }
         Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Toast.makeText(HomePage.this, "Hi", Toast.LENGTH_SHORT).show();
         launcher.launch(takePicture);
     }
     private static Bitmap rotateBitmap(Bitmap img, int degree){
@@ -90,5 +133,11 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener{
         img.recycle();
         return rotatedImage;
     }
-
+//    private static File getOutputMediaFile(){
+//        String fileName = "temp";
+//        String path = Environment.getExternalStorageDirectory().getAbsolutePath()+"/Medicine";
+//        File temp = new File(path,fileName+".jpg");
+//
+//        return temp;
+//    }
 }
